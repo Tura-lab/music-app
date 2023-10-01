25517c97d83fcd15bf06fd8904c754ac5788c693
tura-lab <tiruzertsedeke25@gmail.com>
2023-10-01 18:54:31 +0300

    try deployment error fix

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
 
 // cors allow all origins
 const cors = require('cors');
